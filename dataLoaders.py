cb0983cf31723286882051ea40e9fc2d33bdf2c3
caozheshan-gm <caozheshan123@gmail.com>
2025-03-13 16:50:34 +0800

    token嵌入向量和位置嵌入向量

--- dataLoaders.py
+++ dataLoaders.py
@@ -3,7 +3,7 @@ from GPTDataset import GPTDatasetV1
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-def create_dataloader_v1(txt, batch_size=4, max_length=256,stride=128, shuffle=True, drop_last=True, num_workers=0):
+def create_dataloader_v1(txt, batch_size=4, max_length=256,stride=128, shuffle=False, drop_last=True, num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")                       #A
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)      #B
     dataloader = DataLoader(
@@ -22,14 +22,12 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,stride=128, shuffle=T
 #D 用于预处理的CPU进程数量
 
 
-with open("the-verdict.txt", "r", encoding="utf-8") as f:
-		raw_text = f.read()
+# with open("the-verdict.txt", "r", encoding="utf-8") as f:
+# 		raw_text = f.read()
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
-data_iter = iter(dataloader)              #A
-first_batch = next(data_iter)
-print(first_batch)
+# dataloader = create_dataloader_v1(raw_text, batch_size=4, max_length=4, stride=2)
 
-
-#A 将数据加载器转换为 Python 迭代器，以便通过 Python 的内置 next() 函数获取下一个数据条目。
-#结果 [tensor([[  40,  367, 2885, 1464]]), tensor([[ 367, 2885, 1464, 1807]])]
+# data_iter = iter(dataloader)
+# inputs, targets = next(data_iter)
+# print("Inputs:\n", inputs)
+# print("\nTargets:\n", targets)
